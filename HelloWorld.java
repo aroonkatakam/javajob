d2f81a4a28a116d7e7fb020a5a414615eebee8f3
root <root@ubuntu1.c.aroondigitallync.internal>
2018-02-20 12:37:31 +0000

    changed file for jenkins Job

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-	System.out.println(" Fab now World Munchkins-GITHUB ");
+	System.out.println(" Welcome to jenkins-Github Integration ");
 }
 }
